24f8e99f49d7702a209779fb992642c1e97d03b5
anirban2005143a <23je0104@iitism.ac.in>
2024-10-28 15:18:56 +0530

    dfrgf

--- demo1.cpp
+++ demo1.cpp
@@ -4,50 +4,143 @@ using namespace std;
 class heap
 {
 public:
-  int arr[100] = {0,60,40,30,50,70 ,20};
+  int arr[100] = {0, 60, 40, 30, 50, 70, 20};
   int size = 6;
 
-  void takeInput(int key)
-  {
-    size++;
-    arr[size] = key;
-  }
-
-  void heapify(int i)
-  {
-    int left = 2 * i;
-    int right = 2 * i + 1;
-
-    while ((left <= size || right <= size) && (arr[left] > arr[i] || arr[right] > arr[i]))
-    {
-      if (arr[left] > arr[right])
-      {
-        swap(arr[left], arr[i]);
-        i = left;
-      }
-      else
-      {
-        swap(arr[right], arr[i]);
-        i = right;
-      }
-      left = 2 * i;
-      right = 2 * i + 1;
-    }
-  }
-
-  void insert(int val)
+  void takeInput(int val)
   {
     size++;
     arr[size] = val;
-    int c = size;
-    int p = c / 2;
+  }
 
-    while (c > 1 && arr[p] < arr[c])
+  void heapify_max(int i)
+  {
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left <= size && arr[left] > arr[largest])
     {
-      swap(arr[p], arr[c]);
-      c = p;
-      p = c / 2;
+      largest = left;
     }
+
+    if (right <= size && arr[right] > arr[largest])
+    {
+      largest = right;
+    }
+
+    if (largest != i)
+    {
+      swap(arr[largest], arr[i]);
+      heapify_max(largest);
+    }
+  }
+
+  void heapify_min(int i)
+  {
+    int smallest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left <= size && arr[left] < arr[smallest])
+    {
+      smallest = left;
+    }
+
+    if (right <= size && arr[right] < arr[smallest])
+    {
+      smallest = right;
+    }
+
+    if (smallest != i)
+    {
+      swap(arr[smallest], arr[i]);
+      heapify_min(smallest);
+    }
+  }
+
+  void insert_key_maxHeap(int key)
+  {
+    takeInput(key);
+
+    int child = size;
+    int parent = child / 2;
+    while (child > 1 && arr[parent] < arr[child])
+    {
+      swap(arr[parent], arr[child]);
+      child = parent;
+      parent = child / 2;
+    }
+  }
+
+  void insert_key_minHeap(int key)
+  {
+    takeInput(key);
+
+    int child = size;
+    int parent = child / 2;
+    while (child > 1 && arr[parent] > arr[child])
+    {
+      swap(arr[parent], arr[child]);
+      child = parent;
+      parent = child / 2;
+    }
+  }
+
+  int find_max()
+  {
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n / 2; i >= 1; i--)
+    {
+      heapify_max(i);
+    }
+    return arr[1];
+  }
+
+  int find_min()
+  {
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n / 2; i >= 1; i--)
+    {
+      heapify_min(i);
+    }
+    return arr[1];
+  }
+
+  int extract_max()
+  {
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n / 2; i >= 1; i--)
+    {
+      heapify_max(i);
+    }
+
+    int temp = arr[1];
+    swap(arr[1], arr[size]);
+
+    heapify_max(1);
+
+    size--;
+
+    return temp;
+  }
+
+  int extract_min()
+  {
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n / 2; i >= 1; i--)
+    {
+      heapify_min(i);
+    }
+
+    int temp = arr[1];
+    swap(arr[1], arr[size]);
+
+    heapify_min(1);
+
+    size--;
+
+    return temp;
   }
 
   void print()
@@ -65,16 +158,38 @@ int main()
   heap h;
   int n = 6;
 
-  for (int i = n/2; i >=1 ; i--)
+  // for (int i = n / 2; i >= 1; i--)
+  // {
+  //   h.heapify_max(i);
+  // }
+
+  // h.print();
+
+  // h.insert_key_maxHeap(80);
+
+  // h.print();
+
+  for (int i = n / 2; i >= 1; i--)
   {
-    h.heapify(i);
+    h.heapify_min(i);
   }
-  
-  h.print();
-
-  h.insert(80);
 
   h.print();
+
+  // h.insert_key_minHeap(10);
+
+  // h.print();
+
+  cout << h.find_min() << endl;
+  // cout << h.find_max() << endl;
+
+  // cout << h.extract_max() << endl;
+  cout << h.extract_min() << endl;
+
+  h.print();
+
+  // cout << h.find_max() << endl;
+  cout << h.find_min() << endl;
 
   return 0;
 }
---
+++ heapSort.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class heap
+{
+
+public:
+    int size = 6;
+    int arr[100] = {0, 60, 40, 30, 50, 70, 20};
+
+    void printHeap()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void heapify_max(int i, int n)
+    {
+       
+        int largest = i;
+        int left = 2 * i;
+        int right = 2 * i + 1;
+
+        if (left <= n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right <= n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            swap(arr[largest], arr[i]);
+            heapify_max(largest, n);
+        }
+    }
+
+    void buildHeap(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            size++;
+            cin >> arr[size];
+        }
+        for (int i = n / 2; i >= 1; i--)
+        {
+            heapify_max(i, n);
+        }
+    }
+
+    void non_decreasing(int n)
+    {
+        for (int i = n; i >= 2; i--)
+        {
+            swap(arr[1], arr[i]);
+            heapify_max(1, i-1);
+        }
+    }
+
+    void print()
+    {
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    int n = 6;
+    // cout << "enter no of elements : ";
+    // cin >> n;
+
+    heap h;
+
+    // h.buildHeap(n);
+
+    for (int i = n / 2; i >= 1; i--)
+    {
+        h.heapify_max(i , n);
+    }
+    h.printHeap();
+
+    h.non_decreasing(n);
+
+    h.printHeap();
+
+    return 0;
+}
